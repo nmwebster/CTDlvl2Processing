c001c647e7f7dd4190e91dd0640ebaa66a646139
Nicole Webster <nmwebster@alaska.edu>
2026-02-17 13:11:44 -0900

    skq202510s

---
+++ Settings/SKQ202510S_nmw.m
@@ -0,0 +1,81 @@
+% Settings Script for CTD Project Processing
+
+% DIRECTORY INFORMATION 
+
+PARAMS.indir = 'C:\Users\nmwebster\Documents\projects\CTD\SKQ202510S_CTD';
+PARAMS.outdir = 'C:\Users\nmwebster\Documents\projects\CTD\SKQ202510S_CTD';
+PARAMS.outafname = 'SKQ202510S.ascii';
+PARAMS.outhfname = 'SKQ202510S.hdr';
+
+CTDstatic.indir = 'C:\Users\nmwebster\Documents\projects\CTD\SKQ202510S_CTD';
+CTDstatic.outdir = 'C:\Users\nmwebster\Documents\projects\CTD\SKQ202510S_CTD';
+CTDstatic.outafname = 'SKQ202510S.ascii';
+CTDstatic.outhfname = 'SKQ202510S.hdr';
+
+
+% CTD STATIC PARAMETERS 
+
+PARAMS.vess = 'R/V Sikuliaq';
+PARAMS.cruise = 'SKQ202510S';
+PARAMS.project = 'NGA LTER/Seward Line';
+PARAMS.instrument = 'SBE9';
+PARAMS.pi = 'Danielson';
+PARAMS.agency = 'NSF/EVOSTC/NPRB';
+PARAMS.region = 'Gulf of Alaska';
+PARAMS.datares = 'n';
+PARAMS.timeoff = 0;
+PARAMS.year = 2025 ;
+PARAMS.updown = 0 ;
+PARAMS.interp = 1 ;
+PARAMS.extrap = 0 ;
+PARAMS.eps = 0 ;
+PARAMS.png = 1 ;
+PARAMS.compbot = 1 ;
+PARAMS.compcnv = 1 ;
+PARAMS.pltsel = 0 ;
+PARAMS.nROW = 1 ;
+PARAMS.nCOL = 3 ;
+PARAMS.maxPRESSURE = 1000 ;
+PARAMS.nROW_SS = 2 ;
+PARAMS.nCOL_SS = 4 ;
+PARAMS.maxPRESSURE_SS = 300 ;
+PARAMS.SSsel = 1 ;
+PARAMS.UPdown_SS = 0 ;
+PARAMS.dupeCNV = 1 ;
+PARAMS.addNMEA = 0 ;
+PARAMS.botDep = 0 ;
+PARAMS.botFor = 'Bottom Depth [m]' ;
+PARAMS.depLat = '00.00' ;
+PARAMS.pltDat = 2 ;
+CTDstatic.vess = 'R/V Sikuliaq';
+CTDstatic.cruise = 'SKQ202510S';
+CTDstatic.project = 'NGA LTER/Seward Line';
+CTDstatic.instrument = 'SBE9';
+CTDstatic.pi = 'Danielson';
+CTDstatic.agency = 'NSF/EVOSTC/NPRB';
+CTDstatic.region = 'Gulf of Alaska';
+CTDstatic.datares = 'n';
+CTDstatic.timeoff = 0;
+CTDstatic.year = 2025 ;
+CTDstatic.updown = 0 ;
+CTDstatic.interp = 1 ;
+CTDstatic.extrap = 0 ;
+CTDstatic.eps = 0 ;
+CTDstatic.png = 1 ;
+CTDstatic.compbot = 1 ;
+CTDstatic.compcnv = 1 ;
+CTDstatic.pltsel = 0 ;
+CTDstatic.nROW = 1 ;
+CTDstatic.nCOL = 3 ;
+CTDstatic.maxPRESSURE = 1000 ;
+CTDstatic.nROW_SS = 2 ;
+CTDstatic.nCOL_SS = 4 ;
+CTDstatic.maxPRESSURE_SS = 300 ;
+CTDstatic.SSsel = 1 ;
+CTDstatic.UPdown_SS = 0 ;
+CTDstatic.dupeCNV = 1 ;
+CTDstatic.addNMEA = 0 ;
+CTDstatic.botDep = 0 ;
+CTDstatic.botFor = 'Bottom Depth [m]' ;
+CTDstatic.depLat = '00.00' ;
+CTDstatic.pltDat = 2 ;
